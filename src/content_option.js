95d472c996a7f894ee3571842f87ca87ac248cdf
Bibek chhetri <113200845+isavtars@users.noreply.github.com>
2024-03-27 23:07:11 +0545

    fddf

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+const logotext = "BIBEK";
+const meta = {
+    title: "Bibek Bohora",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Bibek Bohora",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Hi 👋, I'm Bibek Bohora \n A passionate Flutter developer from Nepal",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [
+    
+ 
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value:80,
+    },
+    {
+        name: "Node js",
+        value:80,
+    },
+    {
+        name: "Tailwind css",
+        value: 85,
+    },
+
+    {
+        name: "Dart",
+        value: 85,
+    },
+
+    
+    {
+        name: "Flutter",
+        value: 75,
+    },
+
+    
+
+
+];
+
+const services = [
+    
+    {
+        title: "Mobile Apps",
+        description: "",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bibchhetri5678@gmail.com",
+    YOUR_FONE: "9843111113",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_gihc4pg",
+    YOUR_TEMPLATE_ID: "template_9rzbfdm",
+    YOUR_USER_ID: "Ea4O3Ws-a9n3ze59J",
+};
+
+const socialprofils = {
+    github: "https://github.com/isavtars",
+    facebook: "https://www.facebook.com/bibek.chhetri.73113/",
+    linkedin: "https://www.linkedin.com/in/bibek-bohora-83694227a/?originalSubdomain=np",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
